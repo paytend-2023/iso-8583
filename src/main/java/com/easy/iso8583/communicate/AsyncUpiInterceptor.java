bd7c78df5d8a5e0dd7ae57b959820bbe076ce251
guoliming <guoliming@paytend.com>
2025-04-24 15:29:03 +0800

    优化

--- src/main/java/com/easy/iso8583/communicate/AsyncUpiInterceptor.java
+++ src/main/java/com/easy/iso8583/communicate/AsyncUpiInterceptor.java
@@ -7,22 +7,19 @@ import com.easy.iso8583.ProtocolFactory;
 import com.gd.magic.MagicFactory;
 import com.gd.magic.rmi.RemoteInterceptor;
 import com.gd.magic.rmi.SocketConnect;
-import net.sf.cglib.proxy.MethodProxy;
-import org.apache.log4j.Logger;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.net.Socket;
 import java.text.ParseException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import net.sf.cglib.proxy.MethodProxy;
+import org.apache.log4j.Logger;
 
-/**
- * @author dy_gu king.gu@gmail.com
- * @version V1.0
- * @date 2021/9/14 上午9:03
- * @Copyright: 2021 wepay.mpay.cn Inc. All rights reserved.
- */
 public class AsyncUpiInterceptor extends RemoteInterceptor {
     private static Logger logger = org.apache.log4j.Logger.getLogger(AsyncUpiInterceptor.class);
 
@@ -30,160 +27,171 @@ public class AsyncUpiInterceptor extends RemoteInterceptor {
     private SocketConnect socketConnect;
     private OutputStream outputStream;
     private InputStream inputStream;
-    AsyncBusinessHandle handle = MagicFactory.getProxy(AsyncBusinessHandle.class);
-
-
+    private final ScheduledExecutorService heartbeatScheduler = Executors.newSingleThreadScheduledExecutor();
+    private final AtomicInteger retryCount = new AtomicInteger(0);
+    AsyncBusinessHandle handle = (AsyncBusinessHandle) MagicFactory.getProxy(AsyncBusinessHandle.class);
     private MessageFactory<IsoMessage> factory;
 
     public AsyncUpiInterceptor() {
         try {
-            factory = ProtocolFactory.createUPIFactory("conf/j8583.xml");
+            this.factory = ProtocolFactory.createUPIFactory("conf/j8583.xml");
         } catch (IOException e) {
             logger.debug("read file error :conf/j8583.xml not found");
         }
-        startListen();
+
+        this.startListen();
+        this.startHeartbeat();
+    }
+
+    private void startHeartbeat() {
+        heartbeatScheduler.scheduleAtFixedRate(() -> {
+            try {
+                if (hasInit && outputStream != null) {
+                    synchronized (this) {
+                        outputStream.write(new byte[]{48, 48, 48, 48});
+                        outputStream.flush();
+                        logger.debug("Heartbeat packet sent.");
+                    }
+                }
+            } catch (IOException e) {
+                logger.error("Heartbeat packet delivery failure", e);
+                resetConn();
+            }
+        }, 30, 30, TimeUnit.SECONDS);
     }
 
     void startListen() {
         Thread t = new Thread("upi receive thread") {
             public void run() {
                 while (true) {
-                    if (!hasInit) {
-                        logger.error("startListen socketConnect: " +socketConnect);
-                        logger.error("no init ");
-                        try {
-                            Thread.sleep(1000);
-                            logger.error("no init~waited for 1s");
-                            continue;
-                        } catch (InterruptedException ignore) {
-
-                            //logger.warn("startListen ignore>>"+ignore.);
-                            logger.error("startListen thread :", ignore);
-                        }
+                    if (!AsyncUpiInterceptor.this.hasInit) {
+                        logger.warn("Connection not yet initialized, ready to wait for retry...");
+                        AsyncUpiInterceptor.this.waitBeforeReconnect();
+                        continue;
                     }
-                    try {
-                        final Message msg = TestServer.receiveMessage(inputStream, factory);
 
+                    try {
+                        final Message msg = TestServer.receiveMessage(AsyncUpiInterceptor.this.inputStream, AsyncUpiInterceptor.this.factory);
                         if (Message.NullMessage.equals(msg)) {
-                            logger.warn("receive   heartBeat;");
-                            continue;
-                        }
-                        if (msg == null) {
-                            //logger.warn("receive null message,read error!  ");
-                            //test//todo  resetConn();
-                            continue;
-                        }
-                        MagicFactory.getGlobalThreadPool().execute(
-                                new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        try {
-                                            TestServer.innerHandler(handle, msg);
-                                        } catch (Throwable e) {
-                                            logger.error("business error :", e);
-                                        }
-                                    }
+                            logger.warn("Heartbeat packet received");
+                        } else if (msg != null) {
+                            MagicFactory.getGlobalThreadPool().execute(() -> {
+                                try {
+                                    TestServer.innerHandler(AsyncUpiInterceptor.this.handle, msg);
+                                } catch (Throwable e) {
+                                    logger.error("Business Processing Exception:", e);
                                 }
-                        );
-                    } catch (IOException | ParseException e) {
-                        logger.error(" Communication error ", e);
-                        resetConn();
-                    } catch (Throwable e) {
-                        logger.error("exit receive thread :", e);
+                            });
+                        }
+                    } catch (ParseException | IOException e) {
+                        logger.error("Communication anomaly, preparing to reconnect...", e);
+                        AsyncUpiInterceptor.this.resetConn();
+                    } catch (Throwable ex) {
+                        logger.error("Receive thread exit exception:", ex);
                     }
                 }
-
             }
         };
         t.start();
-        logger.debug("start upi receive thread");
+        logger.debug("Start the upi receiver thread");
     }
 
     void initConn() {
-        if (hasInit) {
-            return;
-        }
-        synchronized (this) {
-            if (!hasInit) {
-                try {
-                    //Close the original connection
-                    if (socketConnect != null) {
-                        socketConnect.setInvalid(true);
-                        closeConnect(socketConnect);
-                    }
-                    //Reacquire Connection
-                    socketConnect = getConnect();
-                    Socket socket = socketConnect.getSocket();
-                    socket.setSoTimeout(0);
-                    inputStream = socket.getInputStream();
-                    outputStream = socket.getOutputStream();
-                    hasInit = true;
-                    logger.debug(">>>initConn sucessfull socketConnect:"+socketConnect);
-                } catch (Exception e) {
-
-                    e.printStackTrace();
-
-                    logger.debug(">>>Exception initConn:"+e);
-
-                    hasInit = false;
-                    if (socketConnect != null) {
-                        socketConnect.setInvalid(true);
-                        try {
-                            closeConnect(socketConnect);
-                        } catch (Exception ignore) {
+        if (!this.hasInit) {
+            synchronized (this) {
+                if (!this.hasInit) {
+                    try {
+                        if (this.socketConnect != null) {
+                            this.socketConnect.setInvalid(true);
+                            this.closeConnect(this.socketConnect);
                         }
-                    }
-                    socketConnect = null;
-                    logger.error("set socketConnect =  " +socketConnect);
 
+                        this.socketConnect = this.getConnect();
+                        Socket socket = this.socketConnect.getSocket();
+                        socket.setSoTimeout(0);
+                        socket.setKeepAlive(true);
+                        this.inputStream = socket.getInputStream();
+                        this.outputStream = socket.getOutputStream();
+                        this.hasInit = true;
+                        retryCount.set(0);
+                        logger.debug(">>>Connection initialized successfully: " + this.socketConnect);
+                    } catch (Exception e) {
+                        logger.error(">>>Connection initialization exception:", e);
+                        this.hasInit = false;
+                        if (this.socketConnect != null) {
+                            this.socketConnect.setInvalid(true);
+                            try {
+                                this.closeConnect(this.socketConnect);
+                            } catch (Exception ignored) {
+                            }
+                        }
+                        this.socketConnect = null;
+                    }
                 }
             }
         }
-
     }
 
     private void resetConn() {
-        hasInit = false;
-        logger.error("resetConn socketConnect:"+socketConnect);
-        //
-        if (socketConnect != null) {
-            socketConnect.setInvalid(true);
+        this.hasInit = false;
+        int count = retryCount.incrementAndGet();
+        logger.warn("resetConn begin " + count + " count，presen socketConnect: " + this.socketConnect);
+        if (this.socketConnect != null) {
+            this.socketConnect.setInvalid(true);
             try {
-				closeConnect(socketConnect);
-				socketConnect = null;
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-		        logger.error("resetConn closeConnect:"+e);
-				e.printStackTrace();
-			}
+                this.closeConnect(this.socketConnect);
+            } catch (Exception e) {
+                logger.error("resetConn closeConnect exceptions:", e);
+            }
+            this.socketConnect = null;
         }
-
     }
 
+    private void waitBeforeReconnect() {
+        int count = retryCount.get();
+        int waitTime = Math.min((int) Math.pow(2, count), 60);
+        try {
+            logger.debug("wait for " + waitTime + " Retry connection in seconds...");
+            Thread.sleep(waitTime * 1000L);
+        } catch (InterruptedException ignored) {
+        }
+    }
 
-    @Override
     public Object intercept(Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
         Message message = (Message) objects[0];
-        logger.error("intercept method = ["+method +"] message=" + message);
-        initConn();
+        logger.info("intercept method = [" + method + "] message=" + message);
+        this.initConn();
+
         try {
             synchronized (this) {
-                if (hasInit) {
-                    if (message == null) {//heartbeat
-                        outputStream.write(new byte[]{0x30, 0x30, 0x30, 0x30});
+                if (this.hasInit) {
+                    if (message == null) {
+                        this.outputStream.write(new byte[]{48, 48, 48, 48});
                     } else {
-                        TestServer.sendIsoMessage(message, outputStream);
+                        TestServer.sendIsoMessage(message, this.outputStream);
                     }
-
                 } else {
-                    logger.error("Connection exception, please resend later!");
+                    logger.error("intercept Abnormal connection. Please try again later.");
                 }
             }
         } catch (IOException e) {
-        	logger.error("intercept write, IOException"+e.getMessage());
-
-            resetConn();
+            logger.error("intercept Failed to send a message, triggering a reconnect:", e);
+            this.resetConn();
         }
+
         return null;
+    }
+
+    public void closeConnect(SocketConnect conn) {
+        try {
+            if (conn != null) {
+                Socket socket = conn.getSocket();
+                if (socket != null && !socket.isClosed()) {
+                    socket.close();
+                }
+            }
+        } catch (IOException e) {
+            logger.error("closeConnect Close Connection Exception:", e);
+        }
     }
 }
